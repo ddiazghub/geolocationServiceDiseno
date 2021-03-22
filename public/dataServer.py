7b785abcba4c3608a960e27e8c1fde0eeb11ff2e
ddiazghub <dedemoya@uninorte.edu.co>
2021-03-21 23:04:35 -0500

    Inclusión de python con postgres

--- public/dataServer.py
+++ public/dataServer.py
@@ -1,66 +1,67 @@
 import socket
 import struct
 from datetime import datetime
-import keyboard
+import os
+import psycopg2
+import sys
+import time
 
 #Se obtiene dirección IP local y se define el puerto para el socket
 port = 50000
 server = socket.gethostbyname(socket.gethostname())
 address = (server, port)
 
-print("\nEl servidor se iniciará en " + str(server) + ":" + str(port) + ". Presione enter para iniciar.")
-keyboard.wait('enter')
+print("\n")
 
 def start():
-    print("\nEl servidor ha iniciado.")
+    delete_last_line()
+    print("El servidor ha iniciado.")
 
-    #Se crea el socket en IP local y puerto 50000
+    #Se crea el socket y se connecta a la base de datos
     UDPSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     UDPSocket.bind(address)
-    #Mientras que el servidor esté habilitado:
-    while True:
-        #Se abre el .txt y se obtiene su contenido
-        loadDataTxt = open("loadData.txt","r")
-        oldDataTxt = loadDataTxt.read()
-        loadDataTxt.close()
+    dbconnection = psycopg2.connect(
+        database = "test",
+        host = os.environ["DB_HOST"],
+        port = os.environ["DB_PORT"],
+        user = os.environ["DB_USERNAME"],
+        password = os.environ["DB_PASSWORD"])
 
-        lines = oldDataTxt.split("\n")
+    dbcursor = dbconnection.cursor()
+#Mientras que el servidor esté habilitado:
+    try:
+        while True:
+            #Se reciben packetes y se guardan los bytes. Se obtiene el ID y se decodifican los valores.
+            (messageBytes, incomingAddress) = UDPSocket.recvfrom(120)
+            vehicleID = messageBytes[:3].hex()
+            [latitude, longitude, timeStamp] = struct.unpack_from('>ffi', messageBytes, 3)
+            dateAndTime = str(datetime.fromtimestamp(timeStamp))
+            messageList = [vehicleID, latitude, longitude, dateAndTime.split(" ")[0], dateAndTime.split(" ")[1]]
 
-        #Se reciben packetes y se guardan los bytes.
-        (messageBytes, incomingAddress) = UDPSocket.recvfrom(120)
+            print("\nRecibido de " + incomingAddress[0] + ":" + str(incomingAddress[1]) + ":\n")
+            print("ID: " + messageList[0] + ", Latitud: " + str(messageList[1]) + ", Longitud: " + str(messageList[2]) + ", Fecha: " + messageList[3] + ", Hora: " + messageList[4])
 
-        print(UDPSocket.recv(120))
-        #Se toman solo los 3 primeros bytes, los cuales son el ID del vehículo.
-        vehicleID = messageBytes[:3].hex()
+            #Se modifica la base de datos con los nuevos datos recibidos.
+            SQL = "UPDATE vehicle SET latitude = %(latitude)s, longitude = %(longitude)s, date = %(date)s, time = %(time)s WHERE id = %(id)s;"
+            dbcursor.execute(SQL, {'id':messageList[0], 'latitude':messageList[1], 'longitude':messageList[2], 'date':messageList[3], 'time':messageList[4]})
+            dbconnection.commit()
+    except:
+        print("\nHa ocurrido un error.")
+        UDPSocket.close()
 
-        #Se decodifican la latitud, la longitud y el timestamp
-        [latitude, longitude, timeStamp] = struct.unpack_from('>ffi', messageBytes, 3)
-        dateAndTime = str(datetime.fromtimestamp(timeStamp))
+def delete_last_line():
+    #Esta función se utiliza para borrar la última línea en la consola
+    sys.stdout.write('\x1b[1A')
+    sys.stdout.write('\x1b[2K')
 
-        #Se guarda todo en un arreglo
-        messageList = [vehicleID, str(latitude), str(longitude), dateAndTime.split(" ")[0], dateAndTime.split(" ")[1]]
+def countdown(seconds):
+    #Cuenta regresiva
+    for s in range(seconds):
+        t = 5 - s
+        delete_last_line()
+        print("El servidor se iniciará en " + str(server) + ":" + str(port) + " en " + str(t) + " segundos...")
+        time.sleep(1)
 
-        print("\nRecibido de " + incomingAddress[0] + ":" + str(incomingAddress[1]) + ":\n")
-        print("ID: " + messageList[0] + ", Latitud: " + messageList[1] + ", Longitud: " + messageList[2] + ", Fecha: " + messageList[3] + ", Hora: " + messageList[4])
-        
-        #Se convierte el arreglo en una string con cada elemento separado por un espacio
-        messageList = " ".join(messageList)
-
-        #Se determina la linea que se editará basado en el ID del paquete recibido
-        if vehicleID == "efee70":
-            lines[0] = messageList
-        elif vehicleID == "487a8d":
-            lines[1] = messageList
-        elif vehicleID == "b7ea25":
-            lines[2] = messageList
-        else:
-            print("Error")
-
-        newDataTxt = "\n".join(lines)
-
-        #Se guarda la nueva información en un .txt
-        loadDataTxt = open("loadData.txt","w")
-        loadDataTxt.write(newDataTxt)
-
-#Se inicia el servidor
+#Se da la cuenta para iniciar el servidor
+countdown(5)
 start()
