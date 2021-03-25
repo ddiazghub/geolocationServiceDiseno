847351c23d148e0b9963c61c72e76bdf7ff72216
ddiazghub <dedemoya@uninorte.edu.co>
2021-03-25 14:09:56 -0500

    s

--- client/src/components/GetVehicles.js
+++ client/src/components/GetVehicles.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useEffect, useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents,  } from 'react-leaflet'
 
 const GetVehicles = () => {
 
@@ -24,25 +24,6 @@ const GetVehicles = () => {
     });
 
     console.log(vehicles);
-
-    function LocationMarker() {
-      const [position, setPosition] = useState(null)
-      const map = useMapEvents({
-        click() {
-          map.locate()
-        },
-        locationfound(e) {
-          setPosition(e.latlng)
-          map.flyTo(e.latlng, map.getZoom())
-        },
-      })
-    
-      return position === null ? null : (
-        <Marker position={position}>
-          <Popup>You are here</Popup>
-        </Marker>
-      )
-    }
 
     return (<Fragment>
       
