c5ccfd8d2d228720b8fa25ed73eb3834c271d4df
ddiazghub <dedemoya@uninorte.edu.co>
2021-03-25 13:23:56 -0500

    Dynamic table

--- client/src/components/GetVehicles.js
+++ client/src/components/GetVehicles.js
@@ -9,7 +9,6 @@ const GetVehicles = () => {
             const response = await fetch("http://34.221.26.86:50001/vehicles");
             const jsonData = await response.json();
 
-            console.log(jsonData);
             setVehicles(jsonData);
         } catch (err) {
             console.error(err.message);
@@ -35,27 +34,16 @@ const GetVehicles = () => {
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <th scope="row" id="plate1">EFE-070</th>
-        <td class="tg-0lax" id="lat">10.992390086364746</td>
-        <td class="tg-0lax" id="lon">-74.80256652832031</td>
-        <td class="tg-0lax" id="fec">2021-03-09</td>
-        <td class="tg-0lax" id="hor">19:05:11</td>
-      </tr>
-      <tr>
-        <th scope="row" id="plate2">ABC-425</th>
-        <td class="tg-0lax" id="latitud">10.992390086364746</td>
-        <td class="tg-0lax" id="longitud">-74.80256652832031</td>
-        <td class="tg-0lax" id="fecha">2021-03-09</td>
-        <td class="tg-0lax" id="hora">19:05:11</td>
-      </tr>
-      <tr>
-        <th scope="row" id="plate3">BTE-425</th>
-        <td class="tg-0lax" id="latitude">10.992390086364746</td>
-        <td class="tg-0lax" id="longitude">-74.80256652832031</td>
-        <td class="tg-0lax" id="date">2021-03-09</td>
-        <td class="tg-0lax" id="time">2021-03-09</td>
-      </tr>
+      
+      {vehicles.map(vehicle => (
+        <tr>
+          <td>{vehicle.id}</th>
+          <td>{vehicle.latitude}</td>
+          <td>{vehicle.longitude}</th>
+          <td>{vehicle.time}</td>
+          <td>{vehicle.date}</td>
+        </tr>
+      ))}
     </tbody>
 </table></Fragment>);
 };
