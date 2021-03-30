0537a670e3626536ea6bade1d15841eeb42c6c37
ddiazghub <dedemoya@uninorte.edu.co>
2021-03-30 00:13:08 +0000

    Error and exception handling

--- dataServer.py
+++ dataServer.py
@@ -18,36 +18,35 @@ def start():
     print("El servidor ha iniciado.")
 
     #Se crea el socket y se connecta a la base de datos
-    UDPSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    UDPSocket.bind(address)
-    dbconnection = psycopg2.connect(
-        database = "test",
-        host = os.environ["DB_HOST"],
-        port = os.environ["DB_PORT"],
-        user = os.environ["DB_USERNAME"],
-        password = os.environ["DB_PASSWORD"])
+    [UDPSocket, dbconnection, dbcursor] = startConnections(address)
 
-    dbcursor = dbconnection.cursor()
 #Mientras que el servidor esté habilitado:
     try:
         while True:
             #Se reciben packetes y se guardan los bytes. Se obtiene el ID y se decodifican los valores.
-            (messageBytes, incomingAddress) = UDPSocket.recvfrom(120)
-            vehicleID = messageBytes[:3].hex()
-            [latitude, longitude, timeStamp] = struct.unpack_from('>ffi', messageBytes, 3)
-            dateAndTime = str(datetime.fromtimestamp(timeStamp))
-            messageList = [vehicleID, latitude, longitude, dateAndTime.split(" ")[0], dateAndTime.split(" ")[1]]
-
-            print("\nRecibido de " + incomingAddress[0] + ":" + str(incomingAddress[1]) + ":\n")
-            print("ID: " + messageList[0] + ", Latitud: " + str(messageList[1]) + ", Longitud: " + str(messageList[2]) + ", Fecha: " + messageList[3] + ", Hora: " + messageList[4])
+            messageList = receiveData(UDPSocket)
 
             #Se modifica la base de datos con los nuevos datos recibidos.
             SQL = "UPDATE vehicle SET latitude = %(latitude)s, longitude = %(longitude)s, date = %(date)s, time = %(time)s WHERE id = %(id)s;"
             dbcursor.execute(SQL, {'id':messageList[0], 'latitude':messageList[1], 'longitude':messageList[2], 'date':messageList[3], 'time':messageList[4]})
             dbconnection.commit()
     except:
-        print("\nHa ocurrido un error.")
+
         UDPSocket.close()
+        dbconnection.close()
+        
+        answer = input("\nHa ocurrido un error. ¿Reiniciar? (S/N): ")
+        
+        yes = answer == "S";
+        no = answer == "N";
+
+        if yes:
+            countdown(5)
+            start()
+        elif no:
+            print("\nEl servidor se ha cerrado.\n")
+        else:
+            print("\nEntrada inválida, el servidor se ha cerrado.\n")
 
 def delete_last_line():
     #Esta función se utiliza para borrar la última línea en la consola
@@ -61,6 +60,30 @@ def countdown(seconds):
         delete_last_line()
         print("El servidor se iniciará en " + str(server) + ":" + str(port) + " en " + str(t) + " segundos...")
         time.sleep(1)
+
+def startConnections(address):
+    UDPSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    UDPSocket.bind(address)
+    dbconnection = psycopg2.connect(
+        database = "test",
+        host = os.environ["DB_HOST"],
+        port = os.environ["DB_PORT"],
+        user = os.environ["DB_USERNAME"],
+        password = os.environ["DB_PASSWORD"])
+        
+    dbcursor = dbconnection.cursor()
+    return UDPSocket, dbconnection, dbcursor
+
+def receiveData(UDPSocket):
+    (messageBytes, incomingAddress) = UDPSocket.recvfrom(120)
+    vehicleID = messageBytes[:3].hex()
+    [latitude, longitude, timeStamp] = struct.unpack_from('>ffi', messageBytes, 3)
+    dateAndTime = str(datetime.fromtimestamp(timeStamp))
+    messageList = [vehicleID, latitude, longitude, dateAndTime.split(" ")[0], dateAndTime.split(" ")[1]]
+
+    print("\nRecibido de " + incomingAddress[0] + ":" + str(incomingAddress[1]) + ":\n")
+    print("ID: " + messageList[0] + ", Latitud: " + str(messageList[1]) + ", Longitud: " + str(messageList[2]) + ", Fecha: " + messageList[3] + ", Hora: " + messageList[4])
+    return messageList
 
 #Se da la cuenta para iniciar el servidor
 countdown(5)
