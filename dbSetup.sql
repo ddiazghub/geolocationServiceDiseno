86d5d158172de68883b4a15bc3ab744770d9e2a4
ddiazghub <dedemoya@uninorte.edu.co>
2021-04-02 20:36:03 -0500

    .

--- dbSetup.sql
+++ dbSetup.sql
@@ -1,24 +1,24 @@
-create table vehicle(
+create table vehicle (
     id char(6) not null primary key,
     latitude float not null,
     longitude float not null,
     tstamp int not null
-)
+);
 
 create table efee70(
 	tstamp int not null primary key,
 	latitude float not null,
 	longitude float not null
-)
+);
 
 create table 487a8d(
 	tstamp int not null primary key,
 	latitude float not null,
 	longitude float not null
-)
+);
 
 create table b7ea25(
 	tstamp int not null primary key,
 	latitude float not null,
 	longitude float not null
-)
+);
