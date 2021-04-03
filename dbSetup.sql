d4c241749319cc48c5b2fd3ef10ee5eb76173b82
ddiazghub <dedemoya@uninorte.edu.co>
2021-04-02 20:50:30 -0500

    .

--- dbSetup.sql
+++ dbSetup.sql
@@ -23,6 +23,6 @@ create table if not exists b7ea25 (
 	longitude float not null
 );
 
-insert into vehicle (id, latitude, longitude, tstamp) values (efee70, 11.027389526367188, -74.86152648925781, 1);
-insert into vehicle (id, latitude, longitude, tstamp) values (487a8d, 10.923937797546387, -74.82032012939453, 1);
-insert into vehicle (id, latitude, longitude, tstamp) values (b7ea25, 10.93842601776123, -74.80989837646484, 1);
+insert into vehicle (id, latitude, longitude, tstamp) values ('efee70', 11.027389526367188, -74.86152648925781, 1);
+insert into vehicle (id, latitude, longitude, tstamp) values ('487a8d', 10.923937797546387, -74.82032012939453, 1);
+insert into vehicle (id, latitude, longitude, tstamp) values ('b7ea25', 10.93842601776123, -74.80989837646484, 1);
