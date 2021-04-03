fbeb7a6c6a3c23e90a2e08c05d4b66d6b995bb14
ddiazghub <dedemoya@uninorte.edu.co>
2021-04-02 20:31:08 -0500

    .

---
+++ dbSetup.sql
@@ -0,0 +1,24 @@
+create table if not exists vehicle(
+    id char(6) not null primary key,
+    latitude float not null,
+    longitude float not null,
+    tstamp int not null
+)
+
+create table if not exists efee70(
+	tstamp int not null primary key,
+	latitude float not null,
+	longitude float not null
+)
+
+create table if not exists 487a8d(
+	tstamp int not null primary key,
+	latitude float not null,
+	longitude float not null
+)
+
+create table if not exists b7ea25(
+	tstamp int not null primary key,
+	latitude float not null,
+	longitude float not null
+)
