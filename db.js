baba05617d02941125636772c5c234e702b91dc6
ddiazghub <dedemoya@uninorte.edu.co>
2021-04-07 16:30:00 -0500

    .

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    database: "test"
+    database: "locationdata"
 });
 
 module.exports = pool;
